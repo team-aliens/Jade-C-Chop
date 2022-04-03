1de589bd74d08800ef83b3e6e893bd9abf1bb428
김대희 <0824dh@naver.com>
2022-04-03 22:51:44 +0900

    🌱 :: SwiftUI Xcode Project

---
+++ SwiftUI/Swift-SwiftUI-Test/Swift-SwiftUI-Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift-SwiftUI-Test
+//
+//  Created by 김대희 on 2022/04/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/Swift-SwiftUI-Test/Swift-SwiftUI-Test/Swift_SwiftUI_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_SwiftUI_TestApp.swift
+//  Swift-SwiftUI-Test
+//
+//  Created by 김대희 on 2022/04/03.
+//
+
+import SwiftUI
+
+@main
+struct Swift_SwiftUI_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
