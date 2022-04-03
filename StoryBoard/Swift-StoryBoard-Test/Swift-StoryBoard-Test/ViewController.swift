da4ff7595f2286018a03894afea04a792c249cd5
김대희 <0824dh@naver.com>
2022-04-04 08:56:38 +0900

    🌧 :: Swift StoryBoard Templet 추가

--- StoryBoard/Swift-StoryBoard-Test/Swift-StoryBoard-Test/ViewController.swift
+++ StoryBoard/Swift-StoryBoard-Test/Swift-StoryBoard-Test/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: 여기에 Oulet 변수를 생성해 주세요
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
+        initialize()
     }
-
-
+    
+    //MARK: 여기에 Action 변수를 생성해 주세요
+    
+    func configureUI() {
+        
+    }
+    func initialize() {
+        
+    }
 }
 
