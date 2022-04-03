1121ece8bf8f0c9130de1b03b9a921ae5fdeca44
김대희 <0824dh@naver.com>
2022-04-04 08:47:02 +0900

    🌧 :: SwiftUI TC 추가

--- SwiftUI/Swift-SwiftUI-Test/Swift-SwiftUI-Test/ContentView.swift
+++ SwiftUI/Swift-SwiftUI-Test/Swift-SwiftUI-Test/ContentView.swift
@@ -2,17 +2,29 @@
 //  ContentView.swift
 //  Swift-SwiftUI-Test
 //
-//  Created by 김대희 on 2022/04/03.
+//  Created by DSM on 2022/04/03.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading) {   // 수직 정렬 (Vertical)
+            Text("Hong Gil Dong")
+                .font(.title)
+            HStack {    // 수평? 정렬 (Horizon)
+                Text("Daedeok Software Meister High School")
+                    .font(.subheadline) // 크기
+                    .foregroundColor(.secondary)    // 색상
+                Spacer()
+                Text("Daejeon")
+                    .font(.subheadline)
+            }
+        }.padding(30)
     }
 }
+
+//MARK: - Previews 보여주는 코드 (오른쪽 휴대폰 화면)
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
