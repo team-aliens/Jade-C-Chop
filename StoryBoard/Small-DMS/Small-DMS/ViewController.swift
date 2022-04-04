5d3c0fbf66d0a6d5035e2662ee5c93c1d9e9b4ce
김대희 <0824dh@naver.com>
2022-04-04 11:06:17 +0900

    🌧 :: Api 코드 추가

--- StoryBoard/Small-DMS/Small-DMS/ViewController.swift
+++ StoryBoard/Small-DMS/Small-DMS/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentDate = ""    // 오늘 날짜 변수
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        getNowDate()
+        getUsers()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func getNowDate() {
+        let dateFormatter = DateFormatter()
+        let date = Date()
+        dateFormatter.dateFormat = "YYYY-MM-dd" // set
+        currentDate = dateFormatter.string(from: date)
+    }
+    
+    private func getUsers() {
+        let url = "https://api.dsm-dms.com/meal/" + currentDate
+        let session = URLSession.shared
+        guard let requestURL = URL(string: url) else {return}
+        session.dataTask(with: requestURL) { data, response, error in
+            if let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 {
+                do {
+                    let jsonSerialization = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: [String: [String]]]
+                    let dateData = jsonSerialization[self.currentDate]
+                    let breakfastData = dateData?["breakfast"]
+                    let lunchData = dateData?["lunch"]
+                    let dinnerData = dateData?["dinner"]
+                    print("breakfast : \(String(describing: breakfastData))\n",
+                          "lunch : \(String(describing: lunchData))\n",
+                          "dinner : \(String(describing: dinnerData))\n")
+                    DispatchQueue.main.async {
+                        // 여기에서 Data를 활용
+                    }
+                }
+            }
+        }.resume()
+    }
+    
 }
-
